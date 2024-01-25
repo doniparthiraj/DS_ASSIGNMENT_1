0fb04a2e6d1728167a5676031b44cf7bd9ef5591
rajkiran <rajkiran@Ubuntu-raj.myguest.virtualbox.org>
2024-01-25 15:18:41 +0530

    beautified changes

--- LoadBalancer/LB.py
+++ LoadBalancer/LB.py
@@ -2,12 +2,14 @@ from flask import Flask,jsonify,redirect,request,url_for
 import os
 import subprocess
 import requests
+from consistent_hash import ConsistentHash as CH
 
 app = Flask(__name__)
-
+hash = CH()
 All_servers = {}
 DOCKER_IMAGE_NAME = "flaskserver"
 DOCKER_API_VERSION = "3.9"
+
 
 #For starting a new server
 def start_new_server(server_id):
@@ -15,27 +17,38 @@ def start_new_server(server_id):
     container_name = f'container{server_id}'
     image_name = DOCKER_IMAGE_NAME
 
-    res = os.popen(f'sudo docker run --name {container_name} --network ds_project_net1 --network-alias {container_name} -d {image_name}').read()
-
-    All_servers[server_id] = container_name   #adding it to the list
+    res = os.popen(f'sudo docker run --name {container_name} --network ds_assignment_1_net1 --network-alias {container_name} -d {image_name}').read()
 
     if len(res) > 0:
+        All_servers[server_id] = container_name
+        hash.add_server_hash(server_id)
         print("Success")
     else:
         raise Exception("Failed to start the server. Check logs for details.")
 
+def checkHeartbeat(server_id):
+    response,code = requests.get(f"http://{All_servers[server_id]}:5000/heart?id={server_id}")
+    return code
+
+def get_avail_serv(cli_id):
+
+    get_ser_id = hash.reqhash(cli_id)
+    while True:
+        if get_ser_id is None:
+            raise Exception("No servers are available")
+        if checkHeartbeat(get_ser_id) == 200:
+            return get_ser_id
+        else:
+            #removecontainer call
+            hash.server_failure(get_ser_id)
 
 @app.route('/<path>',methods=["GET"])
 def path_redirect(path):    
-    
-    container_name = 'container7'
-    id = '106'
     if path == 'home':
-        response = requests.get(f"http://{container_name}:5000/home/{id}")
-        return jsonify(response.json())
-    elif path == 'heartbeat':
-        response = requests.get(f"http://{container_name}:5000/heartbeat")
-        return response.text, response.status_code
+        cli_id = request.args.get('id')
+        server_id = get_avail_serv(cli_id)
+        response = requests.get(f"http://{All_servers[server_id]}:5000/home?id={server_id}")
+        return jsonify(response.json())        
     else:
         response = {
             'message' : f'Error endpoint does not exists -- {path}',
@@ -51,7 +64,7 @@ def add():
         hostnames = data.get('hostnames')
         if n is not None and hostnames is not None and isinstance(hostnames, list):
             for i in range(n):
-                server_id = int(hostnames[i][1:])
+                server_id = hostnames[i]
                 start_new_server(server_id)
             return jsonify({'message': f'Successfully added {n} servers'}), 200
         else:
--- LoadBalancer/consistent_hash.py
+++ LoadBalancer/consistent_hash.py
@@ -1,92 +1,83 @@
 import re
-slot = []
 M = 512
 K = 9
-mappings = {}
-arrange = {}
-reqsList = []
 
-def on_ser_failure(serid):
-    for s in range(0,M):
-        if len(slot[s]) == 0 :
-            continue
-        else:
-            if(slot[s][1] == serid):
-                slot[s] = []
+class ConsistentHash:
 
-def check_next(i):
-    if(len(slot[i]) != 0):
-        curr_i = i
-        while(len(slot[i]) != 0 and i < M):
-            i+=1
-        if( i == M):
-            i = 0
-            while(len(slot[i]) != 0 and i < curr_i):
+    def __init__(self):
+        self.serv_no_to_name = {}
+        self.slot = []
+        for i in range(0,M):
+            self.slot.append([])
+
+    def extract_id(self, serid):
+        match = re.search(r'(\d+)$', serid)
+        if match:
+            ser_id = match.group(1)
+            return int(ser_id)
+
+    def add_server_hash(self, serid):
+        id = self.extract_id(serid)
+        self.serv_no_to_name [id] = serid
+        for j in range(1, K + 1):
+            self.serhash(id, j)
+
+    def check_next(self, i):
+        if(len(self.slot[i]) != 0):
+            curr_i = i
+            while(len(self.slot[i]) != 0 and i < M):
                 i+=1
-            if(i == curr_i):
-                return -1
+            if( i == M):
+                i = 0
+                while(len(self.slot[i]) != 0 and i < curr_i):
+                    i+=1
+                if(i == curr_i):
+                    return -1
+        return i
 
-    return i
+    def serhash(self, serid, vid):
 
-def serhash(serid,vid):
-    v = (serid*serid) + (vid*vid) + (2*vid) + 25
-    i = v%M
+        v = serid**2 + vid**2 + 2*vid + 25
+        i = v % M
 
-    i = check_next(i)
-    if(i == -1):
-        return "No slots are empty"
-    slot[i].append(serid)
-    slot[i].append(vid)
-    
+        i = self.check_next( i )
+        if(i == -1):
+            return "No slots are empty"
+        self.slot[i].append(serid)
+        self.slot[i].append(vid)
+        
+    def recv_server_id(self, i):
 
-def recv_server_id(i):
-
-    found = False
-    server = None
-    for s in range(i+1,M):
-        if len(slot[s]) == 0:
-            continue
-        else:
-            found = True
-            server = tuple(slot[s])
-            return slot[s][0]
-    
-    if found == False:
-        for s in range(0,i):
-            if len(slot[s]) == 0:
+        found = False
+        for s in range(i + 1, M):
+            if len(self.slot[s]) == 0:
                 continue
             else:
                 found = True
-                server = tuple(slot[s])
-                return slot[s][0]
+                return self.slot[s][0]
+        
+        if found == False:
+            for s in range(0, i):
+                if len(self.slot[s]) == 0:
+                    continue
+                else:
+                    found = True
+                    return self.slot[s][0]
 
-    return "No servers added"
+        return "No servers added"
 
-def reqhash(reqid):
-	v = (reqid)*(reqid) + 2*(reqid) + 17
-	i = v%M
-	res = recv_server_id(i)
-	if res == "No servers added":
-		return -1
-	return res
+    def reqhash(self, reqid):
+        v = reqid**2 + 2*reqid + 17
+        i = v % M
+        res = self.recv_server_id(i)
+        if res == "No servers added":
+            return None
+        return self.serv_no_to_name[res]
 
-
-def extract_id(serid):
-	match = re.search(r'(\d+)$', serid)
-	if match:
-		ser_id = match.group(1)
-		return ser_id
-	else:
-		return -1
-
-
-def add_server_hash(serid):
-	for j in range(1,K+1):
-		serhash(serid,j)
-
-def initiate_slot():
-	for i in range(0,M):
-		slot.append([])
-
-
-
+    def on_ser_failure(self, serid):
+        for s in range(0, M):
+            if len(self.slot[s]) == 0 :
+                continue
+            else:
+                if(self.slot[s][1] == serid):
+                    self.slot[s] = []
