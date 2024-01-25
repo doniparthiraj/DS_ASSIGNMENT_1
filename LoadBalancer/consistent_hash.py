ff3c94ab576c1dcab384cbd8ff12068725976223
rajkiran <rajkiran@Ubuntu-raj.myguest.virtualbox.org>
2024-01-25 19:40:31 +0530

    code changes in consisten hash

--- LoadBalancer/consistent_hash.py
+++ LoadBalancer/consistent_hash.py
@@ -17,67 +17,52 @@ class ConsistentHash:
             return int(ser_id)
 
     def add_server_hash(self, serid):
-        id = self.extract_id(serid)
-        self.serv_no_to_name [id] = serid
+        ind = self.extract_id(serid)
+        self.serv_no_to_name [ind] = serid
         for j in range(1, K + 1):
-            self.serhash(id, j)
+            self.serhash(ind, j)
 
-    def check_next(self, i):
-        if(len(self.slot[i]) != 0):
-            curr_i = i
-            while(len(self.slot[i]) != 0 and i < M):
-                i+=1
-            if( i == M):
-                i = 0
-                while(len(self.slot[i]) != 0 and i < curr_i):
-                    i+=1
-                if(i == curr_i):
-                    return -1
-        return i
+    def check_next(self, ind):
+        for i in range(ind,M):
+            if len(self.slot[i]) == 0:
+                return i
+        for i in range(0,ind):
+            if len(self.slot[i]) == 0:
+                return i
+        return -1
 
     def serhash(self, serid, vid):
 
         v = serid**2 + vid**2 + 2*vid + 25
-        i = v % M
+        ind = v % M
 
-        i = self.check_next( i )
-        if(i == -1):
+        ind = self.check_next( ind )
+        if(ind == -1):
             return "No slots are empty"
-        self.slot[i].append(serid)
-        self.slot[i].append(vid)
+        self.slot[ind].append(serid)
+        self.slot[ind].append(vid)
         
-    def recv_server_id(self, i):
+    def recv_server_id(self, ind):
 
-        found = False
-        for s in range(i + 1, M):
-            if len(self.slot[s]) == 0:
-                continue
-            else:
-                found = True
+        for s in range(ind + 1, M):
+            if len(self.slot[s]) != 0:
                 return self.slot[s][0]
-        
-        if found == False:
-            for s in range(0, i):
-                if len(self.slot[s]) == 0:
-                    continue
-                else:
-                    found = True
-                    return self.slot[s][0]
+   
+        for s in range(0, ind):
+            if len(self.slot[s]) != 0:
+                return self.slot[s][0]
 
         return "No servers added"
 
     def reqhash(self, reqid):
         v = reqid**2 + 2*reqid + 17
-        i = v % M
-        res = self.recv_server_id(i)
+        ind = v % M
+        res = self.recv_server_id(ind)
         if res == "No servers added":
             return None
         return self.serv_no_to_name[res]
 
     def on_ser_failure(self, serid):
         for s in range(0, M):
-            if len(self.slot[s]) == 0 :
-                continue
-            else:
-                if(self.slot[s][1] == serid):
-                    self.slot[s] = []
+            if(self.slot[s][0] == serid):
+                self.slot[s] = []
