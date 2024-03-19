b76b29e70db40d3aa2d2e04a2a4fb232e203664d
krishna-kgp <krishna@Ubuntu22.04.myguest.virtualbox.org>
2024-03-19 18:23:33 +0530

    Revert "threading for server write"
    This reverts commit bed20b58b4a051a4ff581ff899e3047580f3d4a4.

--- Server/app.py
+++ Server/app.py
@@ -1,20 +1,6 @@
 from flask import Flask,jsonify,request
 import os
 from helper import SQLHandler
-import threading
-
-
-sh1_lock = threading.Lock()
-sh2_lock = threading.Lock()
-sh3_lock = threading.Lock()
-sh3_lock = threading.Lock()
-
-shard_locks = {
-    "sh1": sh1_lock,
-    "sh2": sh2_lock,
-    "sh3": sh3_lock,
-    "sh4": sh4_lock
-}
 
 db_helper = SQLHandler()
 
@@ -67,11 +53,8 @@ def write():
  
         # Validate the payload structure
         if 'shard' in request_payload and 'curr_idx' in request_payload and 'data' in request_payload:
-            shard_name = request_payload.get('shard', "\0")
-            lock = shard_locks.get(shard_name)
-            with lock:
-                response = db_helper.write_to_database(request_payload)
-                return response
+            response = db_helper.write_to_database(request_payload)
+            return response
  
         return jsonify({"error": "Invalid payload structure"}), 400
  
@@ -88,11 +71,8 @@ def update():
  
         # Validate the payload structure
         if 'shard' in request_payload and 'Stud_id' in request_payload and 'data' in request_payload:
-            shard_name = request_payload.get('shard', "\0")
-            lock = shard_locks.get(shard_name)
-            with lock:
-                response = db_helper.update_to_database(request_payload)
-                return response
+            response = db_helper.update_to_database(request_payload)
+            return response
  
         return jsonify({"error": "Invalid payload structure"}), 400
  
